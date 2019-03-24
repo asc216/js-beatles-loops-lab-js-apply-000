c553a525ad326faff53a4ed3bac53333b8c7f79d
diplomatic-constraint-7525 <andrewsculkin@gmail.com>
2019-03-24 23:20:12 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // add solution here
+function theBeatlesPlay(musicians, instruments){
+  var length1 = musicians.length;
+  var musiciansAndInstruments=[];
+  var i=0;
+  for(i=0;i<=length1;i++){
+    var string1 = musicians[i]+" plays "+instruments[i];
+    musiciansAndInstruments.push(string1);
+  }
+  return musiciansAndInstruments;
+}
+
+function johnLennonFacts(johnFacts){
+  var length1 = johnFacts.length;
+  var i=0;
+  for(i=0;i<=length1;i++){
+    johnFacts[i]=johnFacts[i]+"!!!";
+  }
+  return johnFacts;
+}
+
+function iLoveTheBeatles(num){
+  var empty=[];
+  var i=0;
+  do{
+    empty[i]="I love the Beatles!";
+    i++;
+    num++;
+  }
+  while(num<15)
+  return empty;
+}
